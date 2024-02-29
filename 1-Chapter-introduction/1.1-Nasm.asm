84a0915e9e4134a87568bbfa9d23c2571df2e299
HernaldoMeneses <hernaldomeneses@gmail.com>
2024-02-28 22:23:24 -0300

    1.1-Nasm.asm

---
+++ 1-Chapter-introduction/1.1-Nasm.asm
@@ -0,0 +1,4 @@
+;Netwide Assembler, Nasm, is a 80x86 and x86-64 assembler designed for portability and modulatity.
+;   Supports a range of object file formats
+;       Linux and *BSD a.out, ELF, Mach-O, 16-bit and 32-bit .obj (OMF) format, COFF (including its Win32 and Win64 variants.)
+;       output plain binary files, intel hex and Motorola S-Records formats.
