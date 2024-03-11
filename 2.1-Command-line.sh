577fdeace4fd13ec00cd9213538c6d5ec2ef6b7a
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-11 13:47:48 -0300

    Update 2.1-Command-line.sh

--- 2.1-Command-line.sh
+++ 2.1-Command-line.sh
@@ -2,3 +2,14 @@
 #; to assemble a file
 #; nasm -f <format> <filename> [-o <output>]
 nasm -f elf32 arquivo.asm -o programa
+
+#; to produce a listing file, with the hex codes output
+nasm -f coff myfile.asm -l myfile.lst
+
+#; for help 
+nasm -h
+
+
+#2.1.1 - The -o Option: Specifying the Output File Name
+#21/303
+#
